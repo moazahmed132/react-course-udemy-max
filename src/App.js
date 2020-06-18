30d7e51af58da00c655d0fd5e7f38569595c46d2
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-06-18 10:12:36 +0200

    the children property and how to create a state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App" >
-      <h1 > i am a react app </h1>
-      <Person />
-      <Person name="moaz" age="21" />
-      <Person name="max" age="28" >My Hobby is swimming</Person>
-      <Person name="Dou" age="25" />
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Dou', age: 24 },
+      { name: 'sara', age: 21 }
+    ]
+  }
+  render() {
+    return (
+      <div className="App" >
+        <h1 > i am a react app </h1>
+        <button>Switch Names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >
+          My Hobby is swimming</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 
-    </div >
-  );
-  /* return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I am a react app ya moaz')); */
+      </div >
+    );
+    /* return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I am a react app ya moaz')); */
 
+
+  }
 }
-
-
 export default App; 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,9 @@ import React from 'react'
 const Person = (props) => {
   return (
     <div>
-      <p>I am a person </p>
       <p>I am a {props.name} and I am {props.age} years old </p>
+      <p>{props.children}</p>
+
     </div>
   )
 }
