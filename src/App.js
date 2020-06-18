ada9576384f559b5d05277d0e1acf42f6f9c813a
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-06-18 10:22:01 +0200

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+
+
+const App = (props) => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Dou', age: 24 },
       { name: 'sara', age: 21 }
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         { name: 'moaz', age: 21 },
         { name: 'Dou', age: 24 },
         { name: 'sara', age: 21 }
       ]
     })
-  }
-
-  render() {
-    return (
-      <div className="App" >
-        <h1 > i am a react app </h1>
-        <button onClick={this.switchNameHandler}>Switch Names</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >
-          My Hobby is swimming</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-
-      </div >
-    );
-    /* return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I am a react app ya moaz')); */
-
 
   }
+
+  return (
+    <div className="App" >
+      <h1 > i am a react app </h1>
+      <button onClick={switchNameHandler}>Switch Names</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >
+        My Hobby is swimming</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+
+    </div >
+  );
+  /* return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I am a react app ya moaz')); */
+
 }
+
+
+
 export default App; 
