201b9e38ccb387009f710be9a684c3b574cd1c13
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-06-18 10:24:18 +0200

    passing methods bestween components and it aslo can contain agruments

--- src/App.js
+++ src/App.js
@@ -1,46 +1,43 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-
-
-const App = (props) => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Dou', age: 24 },
       { name: 'sara', age: 21 }
     ]
-  });
-
-
-
-  const switchNameHandler = () => {
-    setPersonsState({
-      persons: [
-        { name: 'moaz', age: 21 },
-        { name: 'Dou', age: 24 },
-        { name: 'sara', age: 21 }
-      ]
-    })
-
   }
 
-  return (
-    <div className="App" >
-      <h1 > i am a react app </h1>
-      <button onClick={switchNameHandler}>Switch Names</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >
-        My Hobby is swimming</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 21 },
+        { name: 'Dou', age: 24 },
+        { name: 'sara', age: 28 }
+      ]
+    })
+  }
+  render() {
+    return (
+      <div className="App" >
+        <h1 > i am a react app </h1>
+        <button onClick={() => this.switchNameHandler('khalaf el dahshory')}>Switch Names</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          switchNameHandler={this.switchNameHandler.bind(this, 'khalaf el wage3')} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >
+          My Hobby is swimming</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 
-    </div >
-  );
-  /* return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I am a react app ya moaz')); */
 
+      </div >
+    );
+    /* return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I am a react app ya moaz')); */
+  }
 }
 
-
-
-export default App; 
+export default App;
